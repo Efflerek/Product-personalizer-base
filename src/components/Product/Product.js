1b449cfaae371e2d5c30e15420ed12d06e6cc0e1
Efflerek <piotr.effler@gmail.com>
2023-06-08 18:48:18 +0100

    Product.js updated

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,46 +1,67 @@
-import styles from './Product.module.scss';
-import clsx from 'clsx';
-import Button from '../Button/Button';
+import styles from "./Product.module.scss";
 
-const Product = props => {
+import PropTypes from "prop-types";
+import { useMemo, useState } from "react";
+import { ProductImage } from "./ProductImage/ProductImage";
+import { ProductTitle } from "./ProductTitle/ProductTitle";
+import { ProductForm } from "./ProductForm/ProductForm";
+
+export const Product = ({ colors, sizes, name, title, basePrice }) => {
+  const [currentColor, setCurrentColor] = useState(colors[0]);
+  const [currentSize, setCurrentSize] = useState(sizes[0].name);
+  const [currentPrice, setCurrentPrice] = useState(sizes[0].additionalPrice);
+
+  const handleActiveClass = (value) => {
+    colors.includes(value) ? setCurrentColor(value) : setCurrentSize(value);
+  };
+
+  const getPrice = useMemo(
+    () => currentPrice + basePrice,
+    [currentPrice, basePrice]
+  );
+
+  const productSummary = (e) => {
+    e.preventDefault();
+
+    const summary = {
+      Name: title,
+      Price: getPrice,
+      Size: currentSize,
+      Color: currentColor,
+    };
+
+    console.table(summary);
+  };
+
   return (
     <article className={styles.product}>
-      <div className={styles.imageContainer}>
-        <img 
-          className={styles.image}
-          alt="Kodilla shirt"
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`} />
-      </div>
+      <ProductImage name={name} title={title} currentColor={currentColor} />
       <div>
-        <header>
-          <h2 className={styles.name}>Kodilla shirt</h2>
-          <span className={styles.price}>Price: 20$</span>
-        </header>
-        <form>
-          <div className={styles.sizes}>
-            <h3 className={styles.optionLabel}>Sizes</h3>
-            <ul className={styles.choices}>
-              <li><button type="button" className={styles.active}>S</button></li>
-              <li><button type="button">M</button></li>
-              <li><button type="button">L</button></li>
-              <li><button type="button">XL</button></li>
-            </ul>
-          </div>
-          <div className={styles.colors}>
-            <h3 className={styles.optionLabel}>Colors</h3>
-            <ul className={styles.choices}>
-              <li><button type="button" className={clsx(styles.colorBlack, styles.active)} /></li>
-              <li><button type="button" className={clsx(styles.colorRed)} /></li>
-              <li><button type="button" className={clsx(styles.colorWhite)} /></li>
-            </ul>
-          </div>
-          <Button className={styles.button}>
-            <span className="fa fa-shopping-cart" />
-          </Button>
-        </form>
+        <ProductTitle title={title} getPrice={getPrice} />
+        <ProductForm
+          sizes={sizes}
+          colors={colors}
+          currentSize={currentSize}
+          currentColor={currentColor}
+          handleActiveClass={handleActiveClass}
+          setCurrentPrice={setCurrentPrice}
+          productSummary={productSummary}
+        />
       </div>
     </article>
-  )
+  );
 };
 
-export default Product;
+Product.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  basePrice: PropTypes.number.isRequired,
+  colors: PropTypes.arrayOf(PropTypes.string),
+  sizes: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      additionalPrice: PropTypes.number,
+    })
+  ),
+};
